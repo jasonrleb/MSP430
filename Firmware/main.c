8ab36f7a5c51da3964e17973bd7328bbed5aca97
Raymond <yuraymond96@gmail.com>
2019-10-25 11:08:56 -0700

    Added ADC and pot
    Unverified

--- Firmware/main.c
+++ Firmware/main.c
@@ -1,28 +1,21 @@
 #include <msp430.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <math.h>
-#include <stdint.h>
+#define RXD BIT5
+#define TXD BIT6
 
 void setClk(void);
-void setTimerB(void);
+void setTimer(void);
 void setUART(void);
+void setADC(void);
+void setPotInput();
 
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-
-    // Set clock
     setClk();
-    // Set timer B
-    setTimerB();
-    // Set UART
+    setTimer();
     setUART();
-
-    // Set P3.0 as test output to verify UART
-    P3DIR |= BIT0;
-    P3OUT &= ~BIT0;
+    setADC();
+    setPotInput();
 
     _EINT(); // enable global interrupts
 
@@ -34,26 +27,26 @@ __interrupt void USCI_A1_ISR(void)
 {
     unsigned char RxByte = 0;
     RxByte = UCA1RXBUF; // get val from RX buffer
-    UCA1TXBUF = RxByte; // "echo back received byte"
-    while (!(UCA1IFG & UCTXIFG)); // wait until the previous Tx is finished
-
-    if (RxByte == 'j')
-        P3OUT ^= BIT0;
-
-    if (RxByte == 'k')
-        P3OUT ^= BIT0;
 }
 
-void setClk(void){
+#pragma vector = ADC10_VECTOR
+__interrupt void ADC10_ISR(void) {
+   ADC10CTL0 &= ~ADC10ENC;
+   ADC10MCTL0 = ADC10SREF_1 + ADC10INCH_12;
+   temperature = ADC10MEM0 >> 2;
+   temperature = temperature - 85;
+   ADC10CTL0 |= ADC10ENC | ADC10SC;
+}
+
+void setClk() {
     CSCTL0_H = CSKEY >> 8; // enables CS registers, can also do = 0xA5 (pg80 ug [ug = user guide])
-//    CSCTL1 &= ~DCORSEL; // DCORSEL set to 0 for low speed ug72
-    CSCTL1 |= DCORSEL // DCORSEL set to 1 for high speed
-    CSCTL1 |= DCOFSEL0 + DCOFSEL1; //High Speed: 24MHz Low Speed: 8MHz 11b
+    CSCTL1 &= ~DCORSEL; // DCORSEL set to 0 ug72
+    CSCTL1 |= DCOFSEL0 + DCOFSEL1; // (pg81 ug) for 8MHz 11b
     CSCTL2 |= SELM0 + SELM1 + SELA0 + SELA1 + SELS0 + SELS1; // set all CLK to run off DCO; (ug82)
-    CSCTL3 &= ~DIVS; // set SMCLK divider to /1
+    CSCTL3 |= DIVS__8; // set SMCLK divider to /8
 }
 
-void setTimerB(void){
+void setTimer() {
     P1DIR |= BIT4 + BIT5;
     P1OUT &= ~(BIT4 + BIT5);
     P1SEL0 |= BIT4 + BIT5;
@@ -65,13 +58,12 @@ void setTimerB(void){
     TB0CCTL2 = OUTMOD_3; // set capture/compare register to set/reset (ug375)
 
     // Set 1000Hz waves (draw up graph to show)
-    TB0CCR0 = 2000 - 1; // = (CLK/divider)/target
-    TB0CCR1 = 1000; // 50% duty cycle
-    TB0CCR2 = 1000; // 25% duty cycle
-
+    TB0CCR0 = 1000 - 1; // = (CLK/divider)/target = (8E6/8)/1000; subtract one since it counts more
+    TB0CCR1 = 500; // 50% duty cycle
+    TB0CCR2 = 500; // 25% duty cycle
 }
 
-void setUART(void){
+void setUART() {
     P2SEL0 &= ~(RXD + TXD); // set to 00 ds74
     P2SEL1 |= RXD + TXD; // set to 11 ds74
     UCA1CTLW0 = UCSSEL0; // 01b for ACLK (pg495 ug)
@@ -81,3 +73,26 @@ void setUART(void){
     UCA1IE |= UCRXIE; // enable UART RX interrupt
 }
 
+void setADC() {
+    // Enable ADC_B (ug449)
+    ADC10CTL0 &= ~ADC10ENC;                        // ensure ENC is clear
+    ADC10CTL0 = ADC10ON + ADC10SHT_3;
+    ADC10CTL1 = ADC10SHS_0 + ADC10SHP + ADC10CONSEQ_0 + ADC10SSEL_0;
+    ADC10CTL2 = ADC10RES; // 8 or 10 bit ADC out
+    ADC10MCTL0 = ADC10SREF_1 + ADC10INCH_12; // ADC10INCH_12 for potentiometer
+    ADC10IV = 0x00;    // clear all ADC12 channel int flags
+    ADC10IE |= ADC10IE0;  // enable ADC10 interrupts
+
+    ADC10CTL0 |= ADC10ENC | ADC10SC; // start the first sample. If this is not done the ADC10 interrupt will not trigger.
+
+    // Set internal voltage reference
+    while(REFCTL0 & REFGENBUSY);
+    REFCTL0 |= REFVSEL_3 + REFON; // ug431
+}
+
+void setPotInput() {
+    // Output Pot on P3.0 ds80
+    P3DIR |= BIT0;
+    P3SEL1 |= BIT0;
+    P3SEL0 |= BIT0;
+}
